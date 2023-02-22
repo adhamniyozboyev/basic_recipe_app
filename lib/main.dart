c569a4c7ea87984b4dfda91f40e39135b3a98aba
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-22 18:30:20 +0500

    add map

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,42 @@ class _HomePageState extends State<HomePage> {
     'Somsa',
     'Lagmon',
   ];
+  Map data = {
+    'Chicken': Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          Icons.access_time,
+          size: 20,
+          color: Colors.grey[300],
+        ),
+        Text(
+          '20 min',
+          style: TextStyle(
+            color: Colors.grey[400],
+          ),
+        ),
+        Icon(
+          Icons.star,
+          size: 20,
+          color: Colors.yellow[700],
+        ),
+        Text('5.0',
+            style: TextStyle(
+              color: Colors.yellow[700],
+            )),
+        Icon(
+          Icons.local_fire_department,
+          size: 20,
+          color: Colors.red[300],
+        ),
+        Text('200 cal',
+            style: TextStyle(
+              color: Colors.red[300],
+            )),
+      ],
+    ),
+  };
   List<Widget> getFood(foods) {
     List<Widget> foodList = [];
     for (var food in foods) {
