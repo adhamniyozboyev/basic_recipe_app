0fdc3e38a3dc2fba3709ddcaca33eb39342f5bce
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-27 18:24:54 +0500

    I set two map in one map

--- lib/foodWidget.dart
+++ lib/foodWidget.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
 
 Map data = {
-  'Chicken': '20 min',
-  'Pizza': '15 min',
-  'Lagmon': '10 min',
-  'Burger': '7 min',
-  'Somsa': '30 min',
-};
-Map star = {
-  'Chicken': '3.2',
-  'Pizza': '3.0',
-  'Lagmon': '3.5',
-  'Burger': '4',
-  'Somsa': '5.0',
-};
-Map ckal = {
-  'Chicken': '200',
-  'Pizza': '300',
-  'Lagmon': '350',
-  'Burger': '180',
-  'Somsa': '500',
+  'Chicken': {'time': '20', 'star': '3.5', 'ckal': '200'},
+  'Pizza': {'time': '15', 'star': '3.0', 'ckal': '250'},
+  'Lagmon': {'time': '10', 'star': '4.0', 'ckal': '230'},
+  'Burger': {'time': '18', 'star': '2.5', 'ckal': '300'},
+  'Somsa': {'time': '30', 'star': '5.0', 'ckal': '500'},
 };
 
 class foodWidget extends StatelessWidget {
@@ -49,7 +35,7 @@ class foodWidget extends StatelessWidget {
             color: Colors.grey[300],
           ),
           Text(
-            data[food],
+            data[food]['time'],
             style: TextStyle(
               color: Colors.grey[400],
             ),
@@ -59,7 +45,7 @@ class foodWidget extends StatelessWidget {
             size: 20,
             color: Colors.yellow[700],
           ),
-          Text(star[food],
+          Text(data[food]['star'],
               style: TextStyle(
                 color: Colors.yellow[700],
               )),
@@ -68,7 +54,7 @@ class foodWidget extends StatelessWidget {
             size: 20,
             color: Colors.red[300],
           ),
-          Text(ckal[food],
+          Text(data[food]['ckal'],
               style: TextStyle(
                 color: Colors.red[300],
               )),
