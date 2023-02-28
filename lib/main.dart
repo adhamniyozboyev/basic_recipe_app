3c071411fd3fae00313fabcc25376c4f98f9f6e4
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-28 08:50:12 +0500

    finish all

--- lib/main.dart
+++ lib/main.dart
@@ -14,48 +14,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List foods = [
+    'Somsa',
     'Chicken',
     'Pizza',
     'Burger',
     'Somsa',
     'Lagmon',
   ];
-  Map data = {
-    'Chicken': Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Icon(
-          Icons.access_time,
-          size: 20,
-          color: Colors.grey[300],
-        ),
-        Text(
-          '20 min',
-          style: TextStyle(
-            color: Colors.grey[400],
-          ),
-        ),
-        Icon(
-          Icons.star,
-          size: 20,
-          color: Colors.yellow[700],
-        ),
-        Text('5.0',
-            style: TextStyle(
-              color: Colors.yellow[700],
-            )),
-        Icon(
-          Icons.local_fire_department,
-          size: 20,
-          color: Colors.red[300],
-        ),
-        Text('200 cal',
-            style: TextStyle(
-              color: Colors.red[300],
-            )),
-      ],
-    ),
-  };
+
   List<Widget> getFood(foods) {
     List<Widget> foodList = [];
     for (var food in foods) {
